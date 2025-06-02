15d5758bffc8aeea36ca7857cc234b98aae2bc1e
Hoarau Erwan <ehoarau003@iutbayonne.univ-pau.fr>
2025-06-02 08:55:58 +0200

    ajout algo visu

--- S2.02_part3/test-img.py
+++ S2.02_part3/test-img.py
@@ -13,10 +13,10 @@ from graphics import *
 os.chdir(r'C:\Users\Hoarau\Desktop\cours\2eme_semestre\S2.02\S2.02_part3\donneesS202')  
 
 # Dictionnaire des successeurs
-with open("dicsucc.json", "r") as fichier:
-    dicSuccCleStr = json.load(fichier)
-dicSucc = {int(k): v for k, v in dicSuccCleStr.items()}
-del dicSuccCleStr
+with open("dicsucc.json", "r") as fichier: # Chargement du fichier JSON
+    dicSuccCleStr = json.load(fichier) # Conversion des clés de chaîne en entiers
+dicSucc = {int(k): v for k, v in dicSuccCleStr.items()} # Conversion des clés de chaîne en entiers
+del dicSuccCleStr # Libération de la mémoire
 
 # Dictionnaire des successeurs avec distance
 with open("dicsuccdist.json", "r") as fichier:
@@ -43,6 +43,30 @@ tabMatPoids = np.array(dfMatricePoids)
 n = len(tabMatPoids)
 lstMatPoids = [[tabMatPoids[i, j] for j in range(n)] for i in range(n)]
 
+
+# ------------------------------
+# Heuristique 
+def heuristique(a, b): # fonction distanceGPS modifiée 
+    
+
+    # Récupération des coordonnées depuis le DataFrame
+    latA = dfSommets.loc[a, 'lat']
+    lonA = dfSommets.loc[a, 'lon']
+    latB = dfSommets.loc[b, 'lat']
+    lonB = dfSommets.loc[b, 'lon']
+
+    # Conversion en radians
+    ltA = radians(latA)
+    ltB = radians(latB)
+    loA = radians(lonA)
+    loB = radians(lonB)
+
+    # Rayon de la Terre en mètres
+    RT = 6378137
+    # Formule du grand cercle
+    S = acos(round(sin(ltA) * sin(ltB) + cos(ltA) * cos(ltB) * cos(abs(loB - loA)), 14))
+
+    return S * RT  # distance en mètres
 
 
 
@@ -117,9 +141,242 @@ def dessiner_graphe(chemin_image="C:/Users/Hoarau/Desktop/cours/2eme_semestre/S2
     return fenetre, positions_pix, lignes
 
 
+# -------------------------------
+# Dijkstra visuel
+import time
+
+def Dijkstra_visuel(depart, arrivee, fenetre, positions_pix, lignes):
+    distances = {point: float('inf') for point in dicSuccDist}
+    distances[depart] = 0
+    precedent = {point: None for point in dicSuccDist}
+    non_visites = list(dicSuccDist.keys())
+
+    while non_visites:
+        u = min(non_visites, key=lambda x: distances[x])
+        if distances[u] == float('inf') or u == arrivee:
+            break
+        non_visites.remove(u)
+        for v, poids in dicSuccDist[u]:
+            d = distances[u] + poids
+            if d < distances[v]:
+                distances[v] = d
+                precedent[v] = u
+
+                # Colorier l’arc visité
+                if (u, v) in lignes:
+                    lignes[(u, v)].setFill("yellow")
+                    lignes[(u, v)].setWidth(3)
+                elif (v, u) in lignes:  # graphe non orienté
+                    lignes[(v, u)].setFill("yellow")
+                    lignes[(v, u)].setWidth(3)
+
+                # Pause pour visualisation
+                time.sleep(0.02)
+
+    # Reconstruction du chemin final
+    chemin = []
+    courant = arrivee
+    while courant is not None:
+        chemin.insert(0, courant)
+        courant = precedent[courant]
+
+    # Colorer le chemin final en bleu
+    for i in range(len(chemin) - 1):
+        u, v = chemin[i], chemin[i+1]
+        if (u, v) in lignes:
+            lignes[(u, v)].setFill("blue")
+            lignes[(u, v)].setWidth(5)
+        elif (v, u) in lignes:
+            lignes[(v, u)].setFill("blue")
+            lignes[(v, u)].setWidth(5)
+        time.sleep(0.07)
+
+    return chemin, distances[arrivee]
+
+
+
+# -------------------------------
+# Bellman visuel
+
+def Bellman_visuel(depart, arrivee, fenetre, positions_pix, lignes):
+    distances = {s: float('inf') for s in dicSuccDist}
+    distances[depart] = 0
+    previous = {s: None for s in dicSuccDist}
+
+    for _ in range(len(dicSuccDist) - 1):
+        for u in dicSuccDist:
+            for v, weight in dicSuccDist[u]:
+                if distances[u] + weight < distances[v]:
+                    distances[v] = distances[u] + weight
+                    previous[v] = u
+
+                    if (u, v) in lignes:
+                        lignes[(u, v)].setFill("purple")
+                        lignes[(u, v)].setWidth(3)
+                    elif (v, u) in lignes:
+                        lignes[(v, u)].setFill("purple")
+                        lignes[(v, u)].setWidth(3)
+
+
+
+    chemin = []
+    current = arrivee
+    while current is not None:
+        chemin.insert(0, current)
+        current = previous[current]
+
+    for i in range(len(chemin) - 1):
+        u, v = chemin[i], chemin[i+1]
+        if (u, v) in lignes:
+            lignes[(u, v)].setFill("pink")
+            lignes[(u, v)].setWidth(5)
+        elif (v, u) in lignes:
+            lignes[(v, u)].setFill("pink")
+            lignes[(v, u)].setWidth(5)
+        time.sleep(0.07)
+
+    return chemin, distances[arrivee]
+
+
+
+# -------------------------------
+# Algorithme de Bellman-Ford-Kalaba visuel
+
+def BFK_visuel(depart, arrivee, fenetre, positions_pix, lignes):
+    n = len(dicSucc)
+    listeDist = [float("inf")] * n
+    listeDist[correspSomInd[depart]] = 0
+    a_traiter = [depart]
+    pred = [None] * n
+
+    for _ in range(n):
+        a_traiter_futur = []
+        for u in a_traiter:
+            u_ind = correspSomInd[u]
+            for v, poids in dicSuccDist[u]:
+                v_ind = correspSomInd[v]
+                if listeDist[v_ind] > listeDist[u_ind] + poids:
+                    listeDist[v_ind] = listeDist[u_ind] + poids
+                    pred[v_ind] = u
+                    a_traiter_futur.append(v)
+
+                    if (u, v) in lignes:
+                        lignes[(u, v)].setFill("red")
+                        lignes[(u, v)].setWidth(3)
+                    elif (v, u) in lignes:
+                        lignes[(v, u)].setFill("red")
+                        lignes[(v, u)].setWidth(3)
+
+        a_traiter = a_traiter_futur
+
+    # Reconstruction du chemin
+    chemin = []
+    courant = arrivee
+    while courant is not None:
+        chemin.insert(0, courant)
+        courant = pred[correspSomInd[courant]]
+
+    # Colorer le chemin final
+    for i in range(len(chemin) - 1):
+        u, v = chemin[i], chemin[i + 1]
+        if (u, v) in lignes:
+            lignes[(u, v)].setFill("orange")
+            lignes[(u, v)].setWidth(5)
+        elif (v, u) in lignes:
+            lignes[(v, u)].setFill("orange")
+            lignes[(v, u)].setWidth(5)
+        time.sleep(0.07)
+
+    return chemin, listeDist[correspSomInd[arrivee]]
+
+
+# -------------------------------
+# A* visuel
+
+def A_Star_visuel(depart, arrivee, fenetre, positions_pix, lignes):
+    if depart not in dicSuccDist or arrivee not in dicSuccDist:
+        raise ValueError("Départ ou arrivée invalide")
+
+    distances = {p: float('inf') for p in dicSuccDist}
+    distances[depart] = 0
+    precedent = {p: None for p in dicSuccDist}
+    f_scores = {p: float('inf') for p in dicSuccDist}
+    f_scores[depart] = heuristique(depart, arrivee)
+
+    non_visites = list(dicSuccDist.keys())
+
+    while non_visites:
+        u = min(non_visites, key=lambda x: f_scores[x])
+        if f_scores[u] == float('inf') or u == arrivee:
+            break
+        non_visites.remove(u)
+
+        for v, poids in dicSuccDist[u]:
+            tentative_g = distances[u] + poids
+            f_v = tentative_g + heuristique(v, arrivee)
+
+            if tentative_g < distances[v]:
+                distances[v] = tentative_g
+                precedent[v] = u
+                f_scores[v] = f_v
+
+                if (u, v) in lignes:
+                    lignes[(u, v)].setFill("green")
+                    lignes[(u, v)].setWidth(3)
+                elif (v, u) in lignes:
+                    lignes[(v, u)].setFill("green")
+                    lignes[(v, u)].setWidth(3)
+
+                time.sleep(0.02)
+
+    chemin = []
+    courant = arrivee
+    while courant is not None:
+        chemin.insert(0, courant)
+        courant = precedent[courant]
+
+    for i in range(len(chemin) - 1):
+        u, v = chemin[i], chemin[i+1]
+        if (u, v) in lignes:
+            lignes[(u, v)].setFill("lightgreen")
+            lignes[(u, v)].setWidth(5)
+        elif (v, u) in lignes:
+            lignes[(v, u)].setFill("lightgreen")
+            lignes[(v, u)].setWidth(5)
+        time.sleep(0.07)
+
+    return chemin, distances[arrivee]
+
 
 def main():
     fenetre, positions_pix, lignes = dessiner_graphe()
+    debut = time.perf_counter()
+    chemin, dist = Dijkstra_visuel(388382398, 1888303671, fenetre, positions_pix, lignes)
+    fin = time.perf_counter()
+    print("Chemin Dijkstra :", chemin)
+    print("Distance Dijkstra :", dist)
+    print(f"Temps d'exécution de Dijkstra visuel : {fin - debut:.6f} secondes")
+    
+    debut = time.perf_counter()
+    chemin, dist = Bellman_visuel(388382398, 1888303671, fenetre, positions_pix, lignes)
+    fin = time.perf_counter()
+    print("Chemin Bellman :", chemin)
+    print("Distance Bellman :", dist)
+    print(f"Temps d'exécution de Bellman visuel : {fin - debut:.6f} secondes")
+
+    debut = time.perf_counter()
+    chemin, dist = BFK_visuel(388382398, 1888303671, fenetre, positions_pix, lignes)
+    fin = time.perf_counter()
+    print("Chemin BFK :", chemin)
+    print("Distance BFK :", dist)
+    print(f"Temps d'exécution de BFK visuel : {fin - debut:.6f} secondes")
+
+    debut = time.perf_counter()
+    chemin, dist = A_Star_visuel(388382398, 1888303671, fenetre, positions_pix, lignes)
+    fin = time.perf_counter()
+    print("Chemin A* :", chemin)
+    print("Distance A* :", dist)
+    print(f"Temps d'exécution de A* visuel : {fin - debut:.6f} secondes")
     fenetre.getMouse()
     fenetre.close()
 
